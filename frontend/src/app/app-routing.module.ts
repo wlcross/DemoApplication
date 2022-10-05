cc2ad51bdc27789e1b33b4a25f9abfb8a5eb5493
wlcross <william589@revature.net>
2022-10-05 11:13:57 -0400

    More Angular Setup

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { ViewProductsComponent } from './view-products/view-products.component';
+import { AddProductComponent } from './add-product/add-product.component';
+
+const routes: Routes = [
+  {path: '', redirectTo: 'view-products', pathMatch: 'full'},
+  {path: 'view-products', component: ViewProductsComponent}, 
+  {path: 'add-product', component: AddProductComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,15 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { ViewProductsComponent } from './view-products/view-products.component';
+import { AddProductComponent } from './add-product/add-product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ViewProductsComponent,
+    AddProductComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule, 
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
