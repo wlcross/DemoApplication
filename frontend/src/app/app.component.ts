0b041ebb58a3a3a77600eabebb7f0fd7507bcee3
wlcross <william589@revature.net>
2022-10-05 13:10:08 -0400

    Added Navbar

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Product Demo';
+
+  constructor(private router:Router) { }
+  
+  
+  viewProducts() {
+    this.router.navigate(["view-products"]);
+  }
+
+  createProduct() {
+    this.router.navigate(["add-product"]);
+  }
 }
+
